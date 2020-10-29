9567c9a5214979e51e833782c06c719a3b2246a4
Paul Chávez <paulchavezromero20@gmail.com>
2020-10-29 17:03:05 -0500

    references.js added

---
+++ src/js/references.js
@@ -0,0 +1,4 @@
+// references
+export const form      = document.querySelector( '#formulario' ),
+             search    = document.querySelector( '#busqueda' ),
+             result    = document.querySelector( '#resultado' );
